06ce57dc3d7a6975e87976980785fdcee2d961ed
MOlexiy <morm.alyosha@gmail.com>
2022-06-17 16:23:24 +0300

    Olexii_Mormul/17062022

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,33 @@
 import { NgModule } from '@angular/core';
+import { AccordionModule } from 'primeng/accordion';
 import { BrowserModule } from '@angular/platform-browser';
+import {FormsModule} from '@angular/forms';
+import {InputTextModule} from 'primeng/inputtext';
+import {PasswordModule} from 'primeng/password';
+import {ButtonModule} from 'primeng/button';
+import { DividerModule } from "primeng/divider";
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { AuthorizationPageComponent } from './authorization-page/authorization-page.component';
+import { LogginSuccessComponent } from './loggin-success/loggin-success.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthorizationPageComponent,
+    LogginSuccessComponent
   ],
   imports: [
-    BrowserModule
+    AccordionModule,
+    BrowserModule,
+    ButtonModule,
+    DividerModule,
+    FormsModule,
+    InputTextModule,
+    HttpClientModule,
+    PasswordModule
   ],
   providers: [],
   bootstrap: [AppComponent]
